f0b0d4440a2d4033d23b21f31c49460fb86f288b
Yousef Shata <62885904+YousefShata@users.noreply.github.com>
2024-09-02 20:01:12 +0300

    first task

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+    constructor(){
+        this.client = redis.createClient();
+        this.isClientConnected = true;
+        this.client.on('error', (err) => {
+            console.log(`Redis client not connected to the server: ${err.message}`);
+            this.isClientConnected = false;
+        });
+        this.client.on('connect', () => {
+            this.isClientConnected = true;
+        });
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.setAsync = promisify(this.client.set).bind(this.client);
+        this.delAsync = promisify(this.client.del).bind(this.client);
+    }
+
+    isAlive() {
+        return this.isClientConnected = true;
+    }
+    async get(key){
+        const value = await this.getAsync(key);
+        return value;
+    }
+    async set(key, val, duration){
+        await this.setAsync(key, val, 'EX', duration);
+    }
+    async del(key){
+        await this.delAsync(key);
+    }
+}
+const redisClient = new RedisClient();
+module.exports = redisClient;
