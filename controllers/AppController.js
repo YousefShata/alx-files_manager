c670ab1951795e0b4e5826db912fbee579c04885
Yousef Shata <62885904+YousefShata@users.noreply.github.com>
2024-09-04 00:23:01 +0300

    WORK

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,13 +3,13 @@ import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
   static async getStats(req, res) {
     const usersNum = await dbClient.nbUsers();
     const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
+    res.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
