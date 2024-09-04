84e949a778ef3291b78df82ae18d9b2d0f2098e8
Yousef Shata <62885904+YousefShata@users.noreply.github.com>
2024-09-04 17:49:47 +0300

    fixing routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import { Router as router } from 'express';
+//import AuthController from '../controllers/AuthController';
+import { Router } from 'express';
+
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+//router.get('/connect', AuthController.getConnect);
+//router.get('/disconnect', AuthController.getDisconnect);
+//router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
