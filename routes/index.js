1f13b5fdae9aeee146eab6209dc213d2b5ab49cf
MostafaAlaa297 <elaskary609@gmail.com>
2024-09-04 14:12:46 -0700

    create first file

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,6 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
